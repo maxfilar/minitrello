e660ae67bbbbf311ea35198c7865309d13a21b29
maxfilar <maxfilar@gmail.com>
2025-07-31 18:03:03 +0300

    top panel & board setup changing

--- Mini Trello/main.js
+++ Mini Trello/main.js
@@ -1,4 +1,7 @@
 "use strict"
+$("body").css({
+    'background-color' : 'gray'
+})
 
 // ================== variables ====================
 const BOARD_STATS = {
@@ -89,6 +92,12 @@ function create_new_board() {
         // remove elements for create
         $("#create_new_board").remove()
         $("#board_create_panel").remove()
+        TRIGGERS.board_create_panel_opened = false
+
+        // create top panel
+        $("body").append(`<div id="top_panel"></div>`)
+        $("#top_panel").append(`<p id="board_name"></p>`)
+        $("#top_panel").append(`<button id="change_board_setup">Change</button>`)
 
         // apply setup
         BOARD_STATS.name = get_name
@@ -98,8 +107,66 @@ function create_new_board() {
     }
 }
 
+function create_setup_change_panel() {
+    if (TRIGGERS.board_create_panel_opened == false) {
+        TRIGGERS.board_create_panel_opened = true
+
+        // create the panel
+        $("body").append('<div id="board_create_panel"></div>')
+        $("#board_create_panel").css({
+            'margin-left' : 'auto',
+            'z-index' : 2
+        })
+
+        // type board name panel
+        $("#board_create_panel").append('<p class="create_panel_text">Type a name of a new board:</p>')
+        $("#board_create_panel").append(`<input type="text" id="board_name_type">`)
+        
+        // background templates
+        $("#board_create_panel").append('<p class="create_panel_text">Choose the background from templates:</p>')
+        $("#board_create_panel").append(`<div id="templates_background_panel"></div>`)
+        for (var i = 0; i < BACKGROUNDS_TEMPLATE_LIST.length; i++) {
+            $("#templates_background_panel").append(`<div class="template_image" id="image_id${i}"></div>`)
+            $(`#image_id${i}`).css({
+                'background-image' : `url(${BACKGROUNDS_TEMPLATE_LIST[i].img})`,
+            })
+            console.log('images are added to templates')
+        }
+
+        // create button
+        $("#board_create_panel").append(`<button id="change_board">Apply</button>`)
+    } else {
+        $("#board_create_panel").remove()
+        TRIGGERS.board_create_panel_opened = false
+    }
+}
+
+function apply_new_changes() {
+    // get setup 
+    let get_name = $("#board_name_type").val()
+    let get_background = undefined
+    for (var i = 0; i < BACKGROUNDS_TEMPLATE_LIST.length; i++) {
+        if (BACKGROUNDS_TEMPLATE_LIST[i].chosen) {
+            get_background = BACKGROUNDS_TEMPLATE_LIST[i].img
+            break
+        }
+    }
+
+    // apply setup
+    if (get_name != "" && get_background != undefined) {
+        // reset
+        $("#board_create_panel").remove()
+        TRIGGERS.board_create_panel_opened = false
+
+        BOARD_STATS.name = get_name
+        BOARD_STATS.background = get_background
+
+        board_setup_apply()
+    }
+}
+
 function board_setup_apply() {
-    $("body").append(`<p id="board_name">${BOARD_STATS.name}</p>`)
+    $("#board_name").text(BOARD_STATS.name)
     $("body").css({
         'background-image' : `url(${BOARD_STATS.background})`
     })
@@ -111,3 +178,7 @@ $("#create_new_board").click(create_new_board_panel)
 $(document).on("click",".template_image",select_image_from_template)
 
 $(document).on("click","#create_board",create_new_board)
+
+$(document).on("click","#change_board_setup",create_setup_change_panel)
+
+$(document).on("click","#change_board",apply_new_changes)
